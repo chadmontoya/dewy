180741326ab4cd828b4c54c7a1ebda1aa8c44252
chadmontoya <chadmontoy@gmail.com>
2025-03-02 13:22:46 -0700

    add shimmer to outfits view

--- dewy/Core/Collections/Views/CollectionCard.swift
+++ dewy/Core/Collections/Views/CollectionCard.swift
@@ -7,159 +7,108 @@ struct CollectionCard: View {
     var screenSize: CGSize
     var collection: Collection
     
+    private var isLoading: Bool {
+        guard let urls = collection.thumbnailUrls else { return false }
+        return !urls.allSatisfy { collectionsVM.loadedImages[$0] != nil }
+    }
+    
     var body: some View {
         GeometryReader { geometry in
             let size = geometry.size
             
             HStack(spacing: 4) {
-                if let firstImageURL = collection.thumbnailUrls?.first {
-                    if let firstImage = collectionsVM.loadedImages[firstImageURL] {
-                        firstImage
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: size.width * 0.5, height: size.height)
-                            .clipShape(
-                                .rect(
-                                    topLeadingRadius: 12,
-                                    bottomLeadingRadius: 12,
-                                    bottomTrailingRadius: 0,
-                                    topTrailingRadius: 0
-                                )
-                            )
-                    } else {
-                        Rectangle()
-                            .fill(.gray.opacity(0.3))
-                            .frame(width: size.width * 0.5, height: size.height)
-                            .clipShape(
-                                .rect(
-                                    topLeadingRadius: 12,
-                                    bottomLeadingRadius: 12,
-                                    bottomTrailingRadius: 0,
-                                    topTrailingRadius: 0
-                                )
-                            )
-                            .shimmering(
-                                animation: .easeInOut(duration: 1)
-                                    .repeatForever(autoreverses: false)
-                            )
-                            .onAppear {
-                                //                                collectionsVM.loadImage(from: firstImageURL)
+                Rectangle()
+                    .fill(.gray.opacity(0.3))
+                    .frame(width: size.width * 0.5, height: size.height)
+                    .clipShape(
+                        .rect(
+                            topLeadingRadius: 12,
+                            bottomLeadingRadius: 12,
+                            bottomTrailingRadius: 0,
+                            topTrailingRadius: 0
+                        )
+                    )
+                    .overlay {
+                        if !isLoading,
+                           let firstImageURL = collection.thumbnailUrls?.first,
+                           let firstImage = collectionsVM.loadedImages[firstImageURL] {
+                            firstImage
+                                .resizable()
+                                .scaledToFill()
+                        } else {
+                            Color.clear.onAppear {
+                                if let url = collection.thumbnailUrls?.first {
+                                    collectionsVM.loadImage(from: url)
+                                }
                             }
+                        }
                     }
-                }
-                else {
+                    .clipped()
+                
+                VStack(spacing: 4) {
                     Rectangle()
                         .fill(.gray.opacity(0.3))
-                        .frame(width: size.width * 0.5, height: size.height)
+                        .frame(height: (size.height - 4) / 2)
                         .clipShape(
                             .rect(
-                                topLeadingRadius: 12,
-                                bottomLeadingRadius: 12,
+                                topLeadingRadius: 0,
+                                bottomLeadingRadius: 0,
                                 bottomTrailingRadius: 0,
+                                topTrailingRadius: 12
+                            )
+                        )
+                        .overlay {
+                            if !isLoading,
+                               let secondImageURL = collection.thumbnailUrls?.dropFirst().first,
+                               let secondImage = collectionsVM.loadedImages[secondImageURL] {
+                                secondImage
+                                    .resizable()
+                                    .scaledToFill()
+                            } else {
+                                Color.clear.onAppear {
+                                    if let url = collection.thumbnailUrls?.dropFirst().first {
+                                        collectionsVM.loadImage(from: url)
+                                    }
+                                }
+                            }
+                        }
+                        .clipped()
+                    
+                    Rectangle()
+                        .fill(.gray.opacity(0.3))
+                        .frame(height: (size.height - 4) / 2)
+                        .clipShape(
+                            .rect(
+                                topLeadingRadius: 0,
+                                bottomLeadingRadius: 0,
+                                bottomTrailingRadius: 12,
                                 topTrailingRadius: 0
                             )
                         )
-                }
-                
-                VStack(spacing: 4) {
-                    if let secondImageURL = collection.thumbnailUrls?.dropFirst().first {
-                        if let secondImage = collectionsVM.loadedImages[secondImageURL] {
-                            secondImage
-                                .resizable()
-                                .scaledToFill()
-                                .frame(height: (size.height - 4) / 2)
-                                .clipShape(
-                                    .rect(
-                                        topLeadingRadius: 0,
-                                        bottomLeadingRadius: 0,
-                                        bottomTrailingRadius: 0,
-                                        topTrailingRadius: 12
-                                    )
-                                )
-                        } else {
-                            Rectangle()
-                                .fill(.gray.opacity(0.3))
-                                .frame(height: (size.height - 4) / 2)
-                                .clipShape(
-                                    .rect(
-                                        topLeadingRadius: 0,
-                                        bottomLeadingRadius: 0,
-                                        bottomTrailingRadius: 0,
-                                        topTrailingRadius: 12
-                                    )
-                                )
-                                .shimmering(
-                                    animation: .easeInOut(duration: 1)
-                                        .repeatForever(autoreverses: false)
-                                )
-                                .onAppear {
-                                    //                                    collectionsVM.loadImage(from: secondImageURL)
+                        .overlay {
+                            if !isLoading,
+                               let thirdImageURL = collection.thumbnailUrls?.dropFirst(2).first,
+                               let thirdImage = collectionsVM.loadedImages[thirdImageURL] {
+                                thirdImage
+                                    .resizable()
+                                    .scaledToFill()
+                            } else {
+                                Color.clear.onAppear {
+                                    if let url = collection.thumbnailUrls?.dropFirst(2).first {
+                                        collectionsVM.loadImage(from: url)
+                                    }
                                 }
+                            }
                         }
-                    } else {
-                        Rectangle()
-                            .fill(.gray.opacity(0.3))
-                            .frame(height: (size.height - 4) / 2)
-                            .clipShape(
-                                .rect(
-                                    topLeadingRadius: 0,
-                                    bottomLeadingRadius: 0,
-                                    bottomTrailingRadius: 0,
-                                    topTrailingRadius: 12
-                                )
-                            )
-                    }
-                    
-                    if let thirdImageURL = collection.thumbnailUrls?.dropFirst(2).first {
-                        if let thirdImage = collectionsVM.loadedImages[thirdImageURL] {
-                            thirdImage
-                                .resizable()
-                                .scaledToFill()
-                                .frame(height: (size.height - 4) / 2)
-                                .clipShape(
-                                    .rect(
-                                        topLeadingRadius: 0,
-                                        bottomLeadingRadius: 0,
-                                        bottomTrailingRadius: 12,
-                                        topTrailingRadius: 0
-                                    )
-                                )
-                        } else {
-                            Rectangle()
-                                .fill(.gray.opacity(0.3))
-                                .frame(height: (size.height - 4) / 2)
-                                .clipShape(
-                                    .rect(
-                                        topLeadingRadius: 0,
-                                        bottomLeadingRadius: 0,
-                                        bottomTrailingRadius: 12,
-                                        topTrailingRadius: 0
-                                    )
-                                )
-                                .shimmering(
-                                    animation: .easeInOut(duration: 1)
-                                        .repeatForever(autoreverses: false)
-                                )
-                                .onAppear {
-                                    //                                    collectionsVM.loadImage(from: thirdImageURL)
-                                }
-                        }
-                    } else {
-                        Rectangle()
-                            .fill(.gray.opacity(0.3))
-                            .frame(height: (size.height - 4) / 2)
-                            .clipShape(
-                                .rect(
-                                    topLeadingRadius: 0,
-                                    bottomLeadingRadius: 0,
-                                    bottomTrailingRadius: 12,
-                                    topTrailingRadius: 0
-                                )
-                            )
-                    }
+                        .clipped()
                 }
                 .frame(width: size.width * 0.5 - 4)
             }
+            .shimmering(
+                active: isLoading,
+                animation: .easeInOut(duration: 1)
+                    .repeatForever(autoreverses: false)
+            )
             .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
     }
